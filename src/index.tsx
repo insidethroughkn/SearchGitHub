681eb392e0a78adfa2abf5efd0ab95cc60dec6fb
gez-local <hugzg@hotmail.com>
2022-12-10 18:08:37 -0800

    add routing initial

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter, RouterProvider, createBrowserRouter } from "react-router-dom";
+import Repositories from './components/Repositories';
+import About from './components/About';
+import Error from './components/Error';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App/>,
+      errorElement: <Error></Error>
+    },
+    {
+      path: "repositories",
+      element: <Repositories></Repositories>
+    },
+    {
+      path: "about",
+      element: <About></About>
+    }
+  ]
+);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
